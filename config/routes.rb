787293342986be98697f75a08845387785022c8f
April Yeats <yeats.april@gmail.com>
2020-09-24 23:12:37 +0200

    Completed challenge: Mister Cocktail -- forgot to add a delete of dose/ingredient pair but it's done now

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :cocktails, only: %i[index new show create edit destroy] do
     resources :doses, only: %i[create]
   end
+
+  resources :doses, only: %i[destroy]
 end
